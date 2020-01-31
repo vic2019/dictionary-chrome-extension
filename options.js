60e0e08bc6348474c1f5447141406b7f0ac402fe
Victor <victor.fan.wang@gmail.com>
2020-01-31 10:10:00 -0800

    Edit option setting script

--- options.js
+++ options.js
@@ -1,27 +1,11 @@
-const notice = document.getElementById('notice');
-document.querySelector('#setKey').addEventListener('click', saveKey);
-document.querySelector('#default').addEventListener('click', event => {
-  chrome.storage.local.remove('key');
-  notice.style.color = '#00cc00';
-  notice.innerHTML = 'Demo key saved!';
-  event.preventDefault();
+browser.storage.local.get('mode').then(result => {
+  if (result.mode === "context") {
+    document.getElementById('context').checked = true;
+  } else {
+    document.getElementById('dblclick').checked = true;
+  }
 });
 
-function saveKey(event) {
-  const key = document.getElementById('api-key').value.trim();
-
-  if (!key) {
-    notice.style.color = '#ff0000';
-    notice.innerHTML = 'Please enter a valid key.';
-    event.preventDefault();
-    return;
-  }
-
-  notice.style.color = '#00cc00';
-  notice.innerHTML = 'Key saved!';
-  chrome.storage.local.set({
-    key: key
-  });
-  event.preventDefault();
-
-}
+document.getElementById('setMode').addEventListener('change', (event) => {
+  browser.storage.local.set({ mode: event.target.value });
+});
